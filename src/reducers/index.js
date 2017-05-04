65415b4dc86b631b0c761340dfba926c102f0c02
Russell Maxfield <russell.maxfield@aggiemail.usu.edu>
2017-05-04 14:39:19 -0600

    add selectors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers, createStore } from 'redux'
-import linkReducer from './link'
+import links, * as fromLink from './link' // Import all the selectors
 // ... import all your reducers here
 
 const reducers = combineReducers({
-  linkReducer,
+  links,
   // ... Put all aditional reducers here
 })
 
@@ -16,3 +16,7 @@ const store = createStore(
 
 
 export default store
+
+// We call getLinks from the 'fromLink' reducer file and pass it a reference to the part of state
+// that links exists at
+export const getLinks = (state) => fromLink.getLinks(state.links)
--- src/reducers/link.js
+++ src/reducers/link.js
@@ -31,3 +31,8 @@ export default function Link(state=initialState, action) {
       return state;
   }
 }
+
+// The state that is passed in is now a refrenece to the state that exists in this file only.
+// This is a very simple example where just return links on the state, but hopefully you can see how
+// this could be usefull if the state was more complicated.
+export const getLinks = (state) => state.links
